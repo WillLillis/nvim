9ce25e7e43814ccfd5cd420105044a8abb226786
WillLillis <wlillis@umass.edu>
2025-01-19 19:46:07 -0500

    fix: add back recording macro message after adding noice

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,3 +1,26 @@
+local msg = ""
+
+local get_msg = function()
+    return msg
+end
+
 require('lualine').setup({
-    options = { theme = "codedark" }
+    options = { theme = "codedark" },
+    sections = {
+        lualine_x = {
+            {
+                get_msg,
+                cond = function()
+                    local noice_api = require("noice").api.status.mode
+                    if noice_api.has() then
+                        msg = noice_api.get()
+                        return string.find(msg, "recording") ~= nil
+                    end
+                    return false
+                end,
+                color = { fg = "#ff9e64" },
+            },
+            'encoding', 'fileformat', 'filetype',
+        },
+    },
 })
